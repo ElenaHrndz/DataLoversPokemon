5919ad4d37de7eb532b4c72c589b6d67945cbbe7
Elena <hrndzelena@gmail.com>
2019-02-26 00:46:30 -0600

    css style, images. html sections
    add images

---
+++ README2.md
@@ -0,0 +1,47 @@
+## DataLovers Pokemon
+
+## Preámbulo
+Pokemon es una franquicia que tiene sus orígenes en un videojuego RGB que debido a su popularidad traspasó plataformas y medios, llegando a la televisión en 1997 y a partir de entonces se posiciona como una de las franquicias más grandes de la compañia Nintendo.
+
+En 2016 es lanzado un nuevo juego de pokemon, para dispositivos móviles, que mediante el uso de realidad aumentada da a los usuarios la posibilidad de atrapar y entrenar pokemons que pueden ser usados posteriormente en diversa batallas. Gracias a esto Nintendo rompe barreras y se da la posibilidad de jugar a quienes no son consumidores de las consolas de nintendo.
+
+## Proceso y decisiones de diseño
+La finalidad de la página es permitir que los usuarios de la app de Pokemon go conocer un poco más del juego y de los pokemons.
+
+### Ideas de prototiado
+
+![sketch_lowfidelity](./src/images/prototipo1.jpg)
+![sketch_lowfidelity](./src/images/prototipo2.jpg)
+
+## Clientes y relación con el producto
+
+Historias de usuarios:
+
+1.  Como usuario novato de Pokemon go quiero aprender más sobre el juego y los diferentes tipos de pokemons.
+
+¿Quiénes son los principales usuarios de producto?
+
+Jugadores novatos o de ocasión que desconocen datos básicos de los pokemons.
+
+¿Cuáles son los objetivos de estos usuarios en relación con el producto?
+
+Aprender sobre el juego
+
+## Problemas y necesidades del usuario
+
+El principal problema para este tipo de usuarios está en adentrarse en un juego del cual no conoce mucho y en el que el flujo de información constante puede llegar a ser abrumador.
+
+
+## Herramientas
+- Git
+- GitHub
+- GitHub Pages
+- Node.js
+- Jest
+- Boostrap
+- JavaScrip
+- Html
+
+
+Elena Hernández
+Lic. en Filosofía  Laboratoria 7ta Generación Sede CDMX
--- src/data.js
+++ src/data.js
@@ -1,63 +1,22 @@
-/*global Map: true*/
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-let pokemonList;
-let pokemonByName = new Map();
-
-// const example = () => {
-//   return 'example';
-// };
-//
-// function pokemonTypes(typesOfPokemon){
-//   var typesToRetrun = ``;
-//
-//   for(var i = 0; i < typesOfPokemon.length ; i++){
-//     if(i > 0){
-//       typesToRetrun +=' y ';
-//     }
-//     typesToRetrun += typesOfPokemon[i].type.name;
-//   }
-//   return typesToRetrun;
-// }
-
 window.pokemon = {
 
-  setPokemonList: () => {
-     const urlAllPokemon = `https://pokemondata-b60b0.firebaseio.com/pokemon.json`
-
-    fetch(urlAllPokemon)
-    .then((response) => {
-      return response.json()
-    })
-    .then((dataPokemon) => {
-      pokemonList = dataPokemon;
-
-      //pokemonByName.Set(key, data);
-      for(var i = 0 ; i < pokemonList.length; i++){
-        pokemonByName.set(pokemonList[i].name.toLowerCase(), i);
-      }
-      // console.log(dataPokemon[0].name);
-      //   area.innerHTML = `
-      //   <div>
-      //     <h1>Tu pokemon es ${dataPokemon.name} y es de tipo ${pokemonTypes(dataPokemon.types)}</h1>
-      //     <img src="${dataPokemon.sprites.front_default}"/>
-      //   </div>
-      //   `;
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  },
-
-  getPokemonByNumber:(numberOfPokemon) => {
+  getPokemonByNumber: (numberOfPokemon) => {
     numberOfPokemon--;
+    const pokemonList = window.main.getPokemonList();
+    // console.log(pokemonList[numberOfPokemon]);
     return pokemonList[numberOfPokemon];
   },
 
-  getPokemonByName:(nameOfPokemon) => {
+  getPokemonByName: (nameOfPokemon) => {
+    const pokemonList = window.main.getPokemonList();
+    const pokemonByName = window.main.getPokemonMap();
     nameOfPokemon = nameOfPokemon.toLowerCase();
     return pokemonList[pokemonByName.get(nameOfPokemon)];
-  }
+  },
 
+  filterByType: (filterBy) => {
+    const pokemonList = window.main.getPokemonList();
+    const result = pokemonList.filter(pokemon => pokemon.type.includes(filterBy));
+    return result;
+  },
 };
