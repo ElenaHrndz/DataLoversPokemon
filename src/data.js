6ccad491308928f3e0473a80c1bf4208e59ed827
Elena <hrndzelena@gmail.com>
2019-02-25 21:07:05 -0600

    function filter, test, fetch
    data.js
    function filter
    
    data.spec.js
    test
    
    main.js
    fetch on main
    funtion filter buttons

--- src/data.js
+++ src/data.js
@@ -1,66 +1,24 @@
-/*global Map: true*/
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-let pokemonList;
-let pokemonByName = new Map();
-
 
 
 window.pokemon = {
 
-  setPokemonList: () => {
-     const urlAllPokemon = `https://pokemondata-b60b0.firebaseio.com/pokemon.json`
-
-    fetch(urlAllPokemon)
-    .then((response) => {
-      return response.json()
-    })
-    .then((dataPokemon) => {
-      pokemonList = dataPokemon;
-
-      //pokemonByName.Set(key, data);
-      for(var i = 0 ; i < pokemonList.length; i++){
-        pokemonByName.set(pokemonList[i].name.toLowerCase(), i);
-      }
-
-      window.main.startAllPokemons();
-      // console.log(dataPokemon[0].name);
-      //   area.innerHTML = `
-      //   <div>
-      //     <h1>Tu pokemon es ${dataPokemon.name} y es de tipo ${pokemonTypes(dataPokemon.types)}</h1>
-      //     <img src="${dataPokemon.sprites.front_default}"/>
-      //   </div>
-      //   `;
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  },
-
-  getPokemonByNumber:(numberOfPokemon) => {
+  getPokemonByNumber: (numberOfPokemon) => {
     numberOfPokemon--;
+    const pokemonList = window.main.getPokemonList();
+    // console.log(pokemonList[numberOfPokemon]);
     return pokemonList[numberOfPokemon];
   },
 
-  getPokemonByName:(nameOfPokemon) => {
+  getPokemonByName: (nameOfPokemon) => {
+    const pokemonList = window.main.getPokemonList();
+    const pokemonByName = window.main.getPokemonMap();
     nameOfPokemon = nameOfPokemon.toLowerCase();
     return pokemonList[pokemonByName.get(nameOfPokemon)];
   },
 
-  getPokemonsOfType: (pokemonType) => {
-    let pokemonsIdsToReturn = [];
-    for(let i = 0; i < pokemonList.length; i++){
-      for(let j = 0; j < pokemonList[i].type.length; j++){
-        if(pokemonType == pokemonList[i].type[j]){
-          pokemonsIdsToReturn.add(i);
-        }
-      }
-    }
-    return pokemonsIdsToReturn;
+  filterByType: (filterBy) => {
+    const pokemonList = window.main.getPokemonList();
+    const result = pokemonList.filter(pokemon => pokemon.type.includes(filterBy));
+    return result;
   },
-
-  getPokemonsAmount: () => {
-    return pokemonList.length;
-  }
 };
